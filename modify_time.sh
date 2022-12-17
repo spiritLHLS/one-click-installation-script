5839610cf2d42d4df81ace8a2e59ef258cc9bc0b
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-12-17 22:24:11 +0800

    Update modify_time.sh

--- modify_time.sh
+++ modify_time.sh
@@ -8,20 +8,19 @@
 head() {
   # 支持系统：Ubuntu 12+，Debian 6+
   ver="2022.12.17"
-  changeLog="一键修复linux网络脚本"
+  changeLog="一键修复本机系统时间"
   clear
   echo "#######################################################################"
-  echo "#                     ${YELLOW}一键修复linux网络脚本${PLAIN}                               #"
+  echo "#                     ${YELLOW}一键修复本机系统时间脚本${PLAIN}                               #"
   echo "# 版本：$ver                                                    #"
   echo "# 更新日志：$changeLog                               #"
   echo "# ${GREEN}作者${PLAIN}: spiritlhl                                                     #"
   echo "# ${GREEN}作仓库${PLAIN}: https://github.com/spiritLHLS/one-click-installation-script #"
   echo "#######################################################################"
   echo "支持系统：Ubuntu 18+，Debian 8+，centos 7+，Fedora，Almalinux 8.5+"
-  echo "1.修复nameserver为google源或cloudflare源"
-  echo "2.尝试修复为IP类型对应的网络优先级(默认IPV4类型，纯V6类型再替换为IPV6类型)"
+  echo "检测修复本机系统时间，如果相差超过300秒的合理范围则校准时间"
   # Display prompt asking whether to proceed with checking and changing
-  read -p "Do you want to proceed with checking and changing nameserver? [y/n] " -n 1 confirm
+  read -p "Do you want to proceed with checking and changing? [y/n] " -n 1 confirm
   echo ""
 
   # Check user's input and exit if they do not want to proceed
