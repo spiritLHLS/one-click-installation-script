7d3a6aa2ca04fc5f2e2f10ba35f97ac6c8e1f867
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-12-17 19:40:28 +0800

    Update rust.sh

--- rust.sh
+++ rust.sh
@@ -9,10 +9,11 @@ ver="2022.12.08"
 changeLog="一键安装rust，加载官方脚本"
 clear
 echo "#############################################################"
-echo -e "#                     ${YELLOW}一键安装rust脚本${PLAIN}                        #"
+echo "#                     ${YELLOW}一键安装rust脚本${PLAIN}                        #"
 echo "# 版本：$ver                                          #"
 echo "# 更新日志：$changeLog                    #"
-echo -e "# ${GREEN}作者${PLAIN}: spiritlhl                                           #"
+echo "# ${GREEN}作者${PLAIN}: spiritlhl                                           #"
+echo "# ${GREEN}作仓库${PLAIN}: https://github.com/spiritLHLS/one-click-installation-script"
 echo "#############################################################"
 echo "支持系统：Ubuntu 18+，Debian 8+，centos 7+，Fedora，Almalinux 8.5+"
 # Display prompt asking whether to proceed with installation
