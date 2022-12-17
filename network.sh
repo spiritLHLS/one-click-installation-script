a2b75b65b994b9ebea8973c3d319aa017de88724
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-12-17 22:26:08 +0800

    Update network.sh

--- network.sh
+++ network.sh
@@ -5,20 +5,20 @@
 
 
 head() {
-  # 支持系统：Ubuntu 12+，Debian 6+
+  # 支持系统：Ubuntu 18+，Debian 8+，centos 7+，Fedora，Almalinux 8.5+
   ver="2022.12.17"
   changeLog="一键修复linux网络脚本"
   clear
   echo "#######################################################################"
-  echo "#                     ${YELLOW}一键修复linux网络脚本${PLAIN}                               #"
+  echo "#                     ${YELLOW}一键修复linux网络脚本${PLAIN}                           #"
   echo "# 版本：$ver                                                    #"
-  echo "# 更新日志：$changeLog                               #"
+  echo "# 更新日志：$changeLog                                     #"
   echo "# ${GREEN}作者${PLAIN}: spiritlhl                                                     #"
   echo "# ${GREEN}作仓库${PLAIN}: https://github.com/spiritLHLS/one-click-installation-script #"
   echo "#######################################################################"
   echo "支持系统：Ubuntu 18+，Debian 8+，centos 7+，Fedora，Almalinux 8.5+"
-  echo "1.修复nameserver为google源或cloudflare源"
-  echo "2.尝试修复为IP类型对应的网络优先级(默认IPV4类型，纯V6类型再替换为IPV6类型)"
+  echo "1.检测ping谷歌有问题修改nameserver为google源或cloudflare源"
+  echo "2.检测ping谷歌还有问题尝试修复为IP类型对应的网络优先级(默认IPV4类型，纯V6类型再替换为IPV6类型)"
   # Display prompt asking whether to proceed with checking and changing
   read -p "Do you want to proceed with checking and changing nameserver? [y/n] " -n 1 confirm
   echo ""
