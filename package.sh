908101c4d930d8651cbd03bf2fbe8e7f23accc0c
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-12-17 21:23:04 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -4,29 +4,32 @@
 #version: 2022.12.17
 
 
-# 支持系统：Ubuntu 12+，Debian 6+
-ver="2022.12.17"
-changeLog="一键修复apt源，加载对应的源"
-clear
-echo "#######################################################################"
-echo "#                     ${YELLOW}一键修复apt源脚本${PLAIN}                               #"
-echo "# 版本：$ver                                                    #"
-echo "# 更新日志：$changeLog                               #"
-echo "# ${GREEN}作者${PLAIN}: spiritlhl                                                     #"
-echo "# ${GREEN}作仓库${PLAIN}: https://github.com/spiritLHLS/one-click-installation-script #"
-echo "#######################################################################"
-echo "支持系统：Ubuntu 12+，Debian 6+"
-echo "1.修复apt源锁死"
-echo "2.修复apt源公钥缺失"
-echo "3.修复替换系统可用的apt源列表，含国内用阿里源，国外用官方源"
-# Display prompt asking whether to proceed with checking
-read -p "Do you want to proceed with checking? [y/n] " -n 1 confirm
-echo ""
+head() {
+  # 支持系统：Ubuntu 12+，Debian 6+
+  ver="2022.12.17"
+  changeLog="一键修复apt源，加载对应的源"
+  clear
+  echo "#######################################################################"
+  echo "#                     ${YELLOW}一键修复apt源脚本${PLAIN}                               #"
+  echo "# 版本：$ver                                                    #"
+  echo "# 更新日志：$changeLog                               #"
+  echo "# ${GREEN}作者${PLAIN}: spiritlhl                                                     #"
+  echo "# ${GREEN}作仓库${PLAIN}: https://github.com/spiritLHLS/one-click-installation-script #"
+  echo "#######################################################################"
+  echo "支持系统：Ubuntu 12+，Debian 6+"
+  echo "0.修复apt源broken损坏"
+  echo "1.修复apt源锁死"
+  echo "2.修复apt源公钥缺失"
+  echo "3.修复替换系统可用的apt源列表，国内用阿里源，国外用官方源"
+  # Display prompt asking whether to proceed with checking
+  read -p "Do you want to proceed with checking? [y/n] " -n 1 confirm
+  echo ""
 
-# Check user's input and exit if they do not want to proceed
-if [ "$confirm" != "y" ]; then
-  exit 0
-fi
+  # Check user's input and exit if they do not want to proceed
+  if [ "$confirm" != "y" ]; then
+    exit 0
+  fi
+}
 
 
 change_debian_apt_sources() {
@@ -217,104 +220,120 @@ change_ubuntu_apt_sources() {
   fi
 }
 
+fix_broken() {
+  # Check if the output of the update contains "--fix-broken install"
+  if apt update | grep -q "--fix-broken install"; then
+    # If it does, run apt --fix-broken install -y
+    apt --fix-broken install -y
+    apt update
+  fi
+}
+
+fix_locked() {
+  if [ $? -ne 0 ]; then
+    echo "The update failed. Attempting to unlock the apt sources..."
+    if [ -f /etc/debian_version ]; then
+      sudo rm /var/lib/apt/lists/lock
+      sudo rm /var/cache/apt/archives/lock
+    elif [ -f /etc/lsb-release ]; then
+      sudo rm /var/lib/dpkg/lock
+      sudo rm /var/cache/apt/archives/lock
+    fi
+    sudo apt update
+    if [ $? -ne 0 ]; then
+      echo "The update still failed. Attempting to fix missing GPG keys..."
+      if [ -f /etc/debian_version ]; then
+        sudo apt-key update
+      elif [ -f /etc/lsb-release ]; then
+        echo "try sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys missing key"
+      fi
+    fi
+  fi
+}
+
+fix_sources() {
+  # Update the package list to pick up the new sources
+  sudo apt update
+
+  # Check the exit status of the update command
+  if [ $? -ne 0 ]; then
+    # Print a message indicating that the update failed
+    echo "The update failed. Attempting to replace the apt sources..."
+
+    # Check if the system is Debian or Ubuntu
+    if [ -f /etc/debian_version ]; then
+      # Replace the current apt sources list with the one at the specified URL
+      #sudo curl -o /etc/apt/sources.list https://raw.githubusercontent.com/spiritLHLS/one-click-installation-script/main/debian.txt
+      # 获取系统版本
+      release=$(lsb_release -cs)
+
+      # 获取源列表中的版本
+      sources_release=$(grep "^deb" /etc/apt/sources.list | head -n1 | cut -d' ' -f3)
+
+      # 如果版本不同，则执行 change_debian_apt_sources 函数
+      if [ "$release" != "$sources_release" ]; then
+        echo "debian source is wrong"
+      fi
+      # Display prompt asking whether to proceed with updating
+      read -p "Do you want to proceed with updating? [y/n] " -n 1 updating
+      echo ""
+
+      # Check user's input and exit if they do not want to proceed
+      if [ "$updating" != "y" ]; then
+        exit 0
+      else
+        change_debian_apt_sources
+      fi
+    elif [ -f /etc/lsb-release ]; then
+      # Replace the current apt sources list with the one at the specified URL
+      # sudo curl -o /etc/apt/sources.list https://raw.githubusercontent.com/spiritLHLS/one-click-installation-script/main/ubuntu.txt
+      # 获取系统版本
+      release=$(lsb_release -cs)
+
+      # 获取源列表中的版本
+      sources_release=$(grep "^deb" /etc/apt/sources.list | head -n1 | cut -d' ' -f3)
+
+      # 如果版本不同，则执行 change_apt_sources 函数
+      if [ "$release" != "$sources_release" ]; then
+        echo "ubuntu source is wrong"
+      fi
+      # Display prompt asking whether to proceed with updating
+      read -p "Do you want to proceed with updating? [y/n] " -n 1 updating
+      echo ""
+
+      # Check user's input and exit if they do not want to proceed
+      if [ "$updating" != "y" ]; then
+        exit 0
+      else
+        change_ubuntu_apt_sources
+      fi
+    else
+      # Print a message indicating that the system is not supported
+      echo "This system is not supported. The apt sources will not be modified."
+    fi
+    # Update the package list again to pick up the new sources
+    sudo apt update
+
+    # Check the exit status of the update command
+    if [ $? -eq 0 ]; then
+      # Print a message indicating that the update was successful
+      echo "The update was successful."
+    else
+      # Print a message indicating that the update failed and suggest other error resolution methods
+      echo "The update failed. You may want to try the following error resolution methods:
+        - Check your internet connection
+        - Check the sources list for errors
+        - Check for package dependencies
+        - Check for disk space issues"
+    fi
+  fi
+}
+
 
 ##############################################################################################################################################
 
 
-sudo apt update
-
-if [ $? -ne 0 ]; then
-  echo "The update failed. Attempting to unlock the apt sources..."
-  if [ -f /etc/debian_version ]; then
-    sudo rm /var/lib/apt/lists/lock
-    sudo rm /var/cache/apt/archives/lock
-  elif [ -f /etc/lsb-release ]; then
-    sudo rm /var/lib/dpkg/lock
-    sudo rm /var/cache/apt/archives/lock
-  fi
-  sudo apt update
-  if [ $? -ne 0 ]; then
-    echo "The update still failed. Attempting to fix missing GPG keys..."
-    if [ -f /etc/debian_version ]; then
-      sudo apt-key update
-    elif [ -f /etc/lsb-release ]; then
-      echo "try sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys missing key"
-    fi
-  fi
-fi
-
-# Update the package list to pick up the new sources
-sudo apt update
-
-# Check the exit status of the update command
-if [ $? -ne 0 ]; then
-  # Print a message indicating that the update failed
-  echo "The update failed. Attempting to replace the apt sources..."
-
-  # Check if the system is Debian or Ubuntu
-  if [ -f /etc/debian_version ]; then
-    # Replace the current apt sources list with the one at the specified URL
-    #sudo curl -o /etc/apt/sources.list https://raw.githubusercontent.com/spiritLHLS/one-click-installation-script/main/debian.txt
-    # 获取系统版本
-    release=$(lsb_release -cs)
-
-    # 获取源列表中的版本
-    sources_release=$(grep "^deb" /etc/apt/sources.list | head -n1 | cut -d' ' -f3)
-
-    # 如果版本不同，则执行 change_debian_apt_sources 函数
-    if [ "$release" != "$sources_release" ]; then
-      echo "debian source is wrong"
-    fi
-    # Display prompt asking whether to proceed with updating
-    read -p "Do you want to proceed with updating? [y/n] " -n 1 updating
-    echo ""
-
-    # Check user's input and exit if they do not want to proceed
-    if [ "$updating" != "y" ]; then
-      exit 0
-    else
-      change_debian_apt_sources
-    fi
-  elif [ -f /etc/lsb-release ]; then
-    # Replace the current apt sources list with the one at the specified URL
-    # sudo curl -o /etc/apt/sources.list https://raw.githubusercontent.com/spiritLHLS/one-click-installation-script/main/ubuntu.txt
-    # 获取系统版本
-    release=$(lsb_release -cs)
-
-    # 获取源列表中的版本
-    sources_release=$(grep "^deb" /etc/apt/sources.list | head -n1 | cut -d' ' -f3)
-
-    # 如果版本不同，则执行 change_apt_sources 函数
-    if [ "$release" != "$sources_release" ]; then
-      echo "ubuntu source is wrong"
-    fi
-    # Display prompt asking whether to proceed with updating
-    read -p "Do you want to proceed with updating? [y/n] " -n 1 updating
-    echo ""
-
-    # Check user's input and exit if they do not want to proceed
-    if [ "$updating" != "y" ]; then
-      exit 0
-    else
-      change_ubuntu_apt_sources
-    fi
-  else
-    # Print a message indicating that the system is not supported
-    echo "This system is not supported. The apt sources will not be modified."
-  fi
-  # Update the package list again to pick up the new sources
-  sudo apt update
-
-  # Check the exit status of the update command
-  if [ $? -eq 0 ]; then
-    # Print a message indicating that the update was successful
-    echo "The update was successful."
-  else
-    # Print a message indicating that the update failed and suggest other error resolution methods
-    echo "The update failed. You may want to try the following error resolution methods:
-      - Check your internet connection
-      - Check the sources list for errors
-      - Check for package dependencies
-      - Check for disk space issues"
-  fi
-fi
+head
+fix_broken
+fix_locked
+fix_sources
