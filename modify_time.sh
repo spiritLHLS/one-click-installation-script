84af8fd3b3c61cd25b3798ffc97b56f267a59adc
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-12-17 19:29:36 +0800

    Update modify_time.sh

--- modify_time.sh
+++ modify_time.sh
@@ -54,3 +54,19 @@ else
     fi
 fi
 
+# 获取当前时间和网络时间
+CURRENT_TIME=$(date +%s)
+NETWORK_TIME=$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)
+NETWORK_TIME_SECONDS=$(date -d "$NETWORK_TIME" +%s)
+
+# 计算时间差
+DIFF=$(($NETWORK_TIME_SECONDS-$CURRENT_TIME))
+
+# 判断时间差是否在允许范围内
+if [ "$DIFF" -lt 300 ] && [ "$DIFF" -gt -300 ]; then
+    # 在允许范围内，时间准确
+    echo "Time on $OS system is accurate."
+else
+    # 不在允许范围内，时间不准确
+    echo "Time on $OS system is NOT accurate. Please check your system time and time zone settings."
+fi
