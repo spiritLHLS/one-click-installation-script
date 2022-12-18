544c34327d24e9f12e4624c54e14c9f6c884a5f3
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-12-18 13:29:13 +0800

    Update check_sudo.sh

--- check_sudo.sh
+++ check_sudo.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
+#by spiritlhl
+#from https://github.com/spiritLHLS/one-click-installation-script
+#version: 2022.12.18
+
+red(){ echo -e "\033[31m\033[01m$1$2\033[0m"; }
+green(){ echo -e "\033[32m\033[01m$1$2\033[0m"; }
+yellow(){ echo -e "\033[33m\033[01m$1$2\033[0m"; }
+reading(){ read -rp "$(green "$1")" "$2"; }
+
+head() {
+  # 支持系统：Ubuntu 12+，Debian 6+
+  ver="2022.12.18"
+  changeLog="修复sudo: unable to resolve host爆错"
+  clear
+  echo "#######################################################################"
+  echo "#           ${YELLOW}修复sudo: unable to resolve host xxx: Name or service not known爆错${PLAIN}                        #"
+  echo "# 版本：$ver                                                    #"
+  echo "# 更新日志：$changeLog                                      #"
+  echo "# ${GREEN}作者${PLAIN}: spiritlhl                                                     #"
+  echo "# ${GREEN}作仓库${PLAIN}: https://github.com/spiritLHLS/one-click-installation-script #"
+  echo "#######################################################################"
+  echo "支持系统：Ubuntu 18+，Debian 8+，centos 7+，Fedora，Almalinux 8.5+"
+  echo "检测修复sudo: unable to resolve host xxx: Name or service not known爆错"
+  # Display prompt asking whether to proceed with fixing
+  reading "Do you want to proceed with fixing? [y/n] " confirm
+  echo ""
+
+  # Check user's input and exit if they do not want to proceed
+  if [ "$confirm" != "y" ]; then
+    exit 0
+  fi
+}
 
 # Check if the hostname is set correctly in /etc/hosts
 HOSTNAME=$(cat /etc/hostname)
@@ -6,49 +38,49 @@ HOSTS_LINE="$(grep $HOSTNAME /etc/hosts)"
 
 if [ -z "$HOSTS_LINE" ]; then
   # Hostname not found in /etc/hosts. Add it.
-  echo "Updating /etc/hosts with hostname: $HOSTNAME"
+  yellow "Updating /etc/hosts with hostname: $HOSTNAME"
   sudo bash -c "echo '127.0.0.1 $HOSTNAME' >> /etc/hosts"
 else
   # Hostname found in /etc/hosts. Check if the IP address is correct.
   HOSTS_IP="$(awk '{print $1}' <<<$HOSTS_LINE)"
   if [ "$HOSTS_IP" != "127.0.0.1" ]; then
     # IP address is incorrect. Update it.
-    echo "Updating IP address for $HOSTNAME in /etc/hosts"
+    yellow "Updating IP address for $HOSTNAME in /etc/hosts"
     sudo sed -i "s/$HOSTS_IP/127.0.0.1/g" /etc/hosts
   else
     # Hostname and IP address are correct. No changes needed.
-    echo "Hostname and IP address in /etc/hosts are correct."
+    green "Hostname and IP address in /etc/hosts are correct."
   fi
 fi
 
 # Check if the sudo command works
-sudo echo "Testing sudo command..."
+sudo yellow "Testing sudo command..."
 if [ $? -eq 0 ]; then
   # Sudo command works. Exit the script.
-  echo "Fix successful. Exiting script."
+  green "Fix successful. Exiting script."
 else
   # Sudo command failed. Try restarting the networking interface.
-  echo "Sudo command still failing. Restarting networking interface."
+  yellow "Sudo command still failing. Restarting networking interface."
   sudo service networking restart
 
   # Check if the sudo command works after restarting the networking interface
   sudo echo "Testing sudo command after restarting networking interface..."
   if [ $? -eq 0 ]; then
     # Sudo command works. Exit the script.
-    echo "Fix successful. Exiting script."
+    green "Fix successful. Exiting script."
   else
     # Sudo command failed. Try restarting the DNS server.
-    echo "Sudo command still failing. Restarting DNS server."
+    yellow "Sudo command still failing. Restarting DNS server."
     sudo service bind9 restart
 
     # Check if the sudo command works after restarting the DNS server
     sudo echo "Testing sudo command after restarting DNS server..."
     if [ $? -eq 0 ]; then
       # Sudo command works. Exit the script.
-      echo "Fix successful. Exiting script."
+      green "Fix successful. Exiting script."
     else
       # Sudo command still failing. Exiting script.
-      echo "Unable to fix problem. Exiting script."
+      red "Unable to fix problem. Exiting script."
     fi
   fi
 fi
