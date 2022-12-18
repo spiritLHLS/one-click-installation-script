572bf058491b97e71bc67584d16eb5ea07339159
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-12-18 23:23:19 +0800

    Update jupyter.sh

--- jupyter.sh
+++ jupyter.sh
@@ -21,8 +21,7 @@ echo "# ${GREEN}作者${PLAIN}: spiritlhl                                       
 echo "# ${GREEN}作仓库${PLAIN}: https://github.com/spiritLHLS/one-click-installation-script #"
 echo "#######################################################################"
 echo "支持系统：Ubuntu 18+，Debian 8+，centos 7+，Fedora，Almalinux 8.5+"
-echo "有安装则提示是否需要修改用户名和密码，否则则自动安装，自动安装后默认用户名和密码都是spiritlhl，如果安装完毕需要修改，再次执行本脚本修改"
-echo "最后都会打印jupyter的信息，如果本机最后有jupyter的话，无论是通过何种途径安装的"
+echo "执行脚本，之前有安装过则打印设置的登陆信息，没安装过则进行安装再打印信息"
 echo "如果是初次安装无脑输入yes或y回车即可"
 
 
@@ -78,110 +77,63 @@ install_jupyter() {
   nohup jupyter lab --port 13692 --no-browser --ip=0.0.0.0 --allow-root & echo $!
   sleep 5
   cat nohup.out
-}
+  
+  # Add the specified paths to the PATH variable
+  paths="./miniconda3/envs/jupyter-env/etc/jupyter:./miniconda3/envs/jupyter-env/bin/jupyter:./miniconda3/envs/jupyter-env/share/jupyter"
+  export PATH="$paths:$PATH"
 
-change_username_and_password() {
-  # Prompt the user for a new username and password
-  reading "Enter a new username: " username
-  reading "Enter a new password: " password
+  # Remove duplicate paths from the PATH variable
+  new_path=$(echo "$PATH" | tr ':' '\n' | awk '!x[$0]++' | tr '\n' ':')
+  export PATH="$new_path"
 
-  # Check if Python 3 is available
-  if command -v python3 &> /dev/null; then
-    # Python 3 is installed, use it
-    password_hash=$(python3 -c "from notebook.auth import passwd; print(passwd('$password'))")
-  else
-    # Python 3 is not installed, use Python 2
-    password_hash=$(python -c "from notebook.auth import passwd; print(passwd('$password'))")
-  fi
-
-  # Find the jupyter configuration file
-  config_file=$(jupyter --config-dir)/jupyter_notebook_config.py
-  if [ ! -f "$config_file" ]; then
-    jupyter notebook --generate-config
-  fi
-
-  # Update the jupyter configuration file with the new username and password hash
-  sed -i "s/#c.NotebookApp.password = .*/c.NotebookApp.password = u'$password_hash'/" "$config_file"
-  sed -i "s/#c.NotebookApp.username = .*/c.NotebookApp.username = u'$username'/" "$config_file"
+  # Refresh the current shell
+  source ~/.bashrc
+  
+  green "已安装jupyter lab的web端到外网端口13692上，请打开你的 外网IP:13692"
+  green "同时已保存日志输出到当前目录的nohup.out中且已打印5秒日志如上"
+  green "如果需要进一步查询，请关闭本窗口开一个新窗口再执行本脚本，否则无法加载一些预设的环境变量" && exit 0
 }
 
 query_jupyter_info() {
+  source activate jupyter-env
   # Check if jupyter is installed
-  if jupyter --version &> /dev/null; then
+  if ! jupyter --version &> /dev/null; then
     echo "Error: Jupyter is not installed on this system."
     return 1
   fi
 
-  # Check if jupyter_notebook_config.py exists
-  if [ ! -f ~/.jupyter/jupyter_notebook_config.py ]; then
-    echo "Error: jupyter_notebook_config.py not found."
+  # Find jupyter config directory
+  config_dir=$(jupyter --config-dir)
+  config_path="$config_dir/jupyter_server_config.py"
+
+  # Check if jupyter_server_config.py exists
+  if [ ! -f "$config_path" ]; then
+    echo "Error: jupyter_server_config.py not found."
     return 1
   fi
 
-  # Read jupyter_notebook_config.py
-  config=$(cat ~/.jupyter/jupyter_notebook_config.py)
+  # Read jupyter_server_config.py
+  config=$(cat "$config_path")
 
-  # Extract username and password
-  password_required=$(echo "$config" | grep "c.NotebookApp.password_required" | awk -F "=" '{print $2}' | tr -d ' ')
-  if [ "$password_required" = "True" ]; then
-    username=$(echo "$config" | grep "c.NotebookApp.password" | awk -F "=" '{print $2}' | tr -d ' ' | base64 --decode | awk -F ":" '{print $1}')
-    password=$(echo "$config" | grep "c.NotebookApp.password" | awk -F "=" '{print $2}' | tr -d ' ' | base64 --decode | awk -F ":" '{print $2}')
-    echo "Username: $username"
-    echo "Password: $password"
-  else
-    echo "Jupyter Notebook server is not password protected."
+  # Extract token
+  # token=$(echo "$config" | grep "c.ServerApp.token" | awk -F "=" '{print $2}' | tr -d ' ')
+  token=$(echo "$config" | grep "c.IdentityProvider.token" | awk -F "=" '{print $2}' | tr -d ' ')
+  if [ -z "$token" ]; then
+    echo "Error: Token not found in jupyter_server_config.py."
+    return 1
   fi
+  echo "Token: $token"
 
   # Extract port
-  port=$(echo "$config" | grep "c.NotebookApp.port" | awk -F "=" '{print $2}' | tr -d ' ')
+  port=$(echo "$config" | grep "c.ServerApp.port" | awk -F "=" '{print $2}' | tr -d ' ')
   echo "Port: $port"
-  
-  
 }
-
-uninstall_jupyter() {
-  # Deactivate the virtual environment
-  deactivate
-
-  # Remove the virtual environment
-  rm -rf jupyter-env
-
-  # Uninstall Jupyter Notebook and Jupyter Lab
-  pip3 uninstall jupyter jupyterlab
-
-  # Remove Jupyter Notebook from PATH
-  sed -i '/export PATH="$PATH:$(python3 -m site --user-base)\/bin"/d' ~/.bashrc
-  source ~/.bashrc
-
-  # Remove Jupyter Notebook config files
-  rm -rf ~/.jupyter
-
-  # Remove port 13692 from firewall
-  if command -v ufw &> /dev/null; then
-      sudo ufw delete allow 13692/tcp
-  elif command -v firewall-cmd &> /dev/null; then
-      sudo firewall-cmd --remove-port=13692/tcp --permanent
-      sudo firewall-cmd --reload
-  fi
-
-}
-
-
 
 main() {
+  source activate jupyter-env
   # Check if jupyter is installed
   if jupyter --version &> /dev/null; then
-    echo "Jupyter is already installed on this system."
-    reading "Do you want to change the username and password for Jupyter? (y/n) " change
-    echo ""
-
-    # Check user's input and exit if they do not want to proceed
-    if [ "$change" != "y" ]; then
-      exit 0
-    fi
-    
-    change_username_and_password
-    
+    green "Jupyter is already installed on this system."
   else
     reading "Jupyter is not installed on this system. Do you want to install it? (y/n) " confirminstall
     echo ""
@@ -190,9 +142,7 @@ main() {
     if [ "$confirminstall" != "y" ]; then
       exit 0
     fi
-    
     install_jupyter
-    
   fi
   
   # Print the current info for Jupyter
