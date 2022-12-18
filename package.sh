25d3a0c2e67cd8aff7bac1504428834480e2dd2e
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-12-18 17:13:01 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -47,123 +47,36 @@ change_debian_apt_sources() {
   # Determine Debian version
   DEBIAN_VERSION=$(lsb_release -sr)
 
-  if [ "$location" = "China" ]; then
-    # IP is in China, update apt-get sources
-    echo "IP is in China, updating apt-get sources."
-    if [ "$DEBIAN_VERSION" = "6.0" ]; then
-      # Debian 6
-      cat > /etc/apt/sources.list <<EOF
-deb http://mirrors.aliyun.com/debian/ squeeze main non-free contrib
-deb-src http://mirrors.aliyun.com/debian/ squeeze main non-free contrib
-EOF
-    elif [ "$DEBIAN_VERSION" = "7.0" ]; then
-      # Debian 7
-      cat > /etc/apt/sources.list <<EOF
-deb http://mirrors.aliyun.com/debian/ wheezy main non-free contrib
-deb-src http://mirrors.aliyun.com/debian/ wheezy main non-free contrib
-EOF
-    elif [ "$DEBIAN_VERSION" = "8.0" ]; then
-      # Debian 8
-      cat > /etc/apt/sources.list <<EOF
-deb http://mirrors.aliyun.com/debian/ jessie main non-free contrib
-deb-src http://mirrors.aliyun.com/debian/ jessie main non-free contrib
-EOF
-    elif [ "$DEBIAN_VERSION" = "9.0" ]; then
-      # Debian 9
-      cat > /etc/apt/sources.list <<EOF
-deb http://mirrors.aliyun.com/debian/ stretch main non-free contrib
-deb-src http://mirrors.aliyun.com/debian/ stretch main non-free contrib
-EOF
-    elif [ "$DEBIAN_VERSION" = "10.0" ]; then
-      # Debian 10
-      cat > /etc/apt/sources.list <<EOF
-deb http://mirrors.aliyun.com/debian/ buster main non-free contrib
-deb-src http://mirrors.aliyun.com/debian/ buster main non-free contrib
-EOF
-    elif [ "$DEBIAN_VERSION" = "11.0" ]; then
-      # Debian 11
-      cat > /etc/apt/sources.list <<EOF
-deb http://mirrors.aliyun.com/debian/ bullseye main non-free contrib
-deb-src http://mirrors.aliyun.com/debian/ bullseye main non-free contrib
-EOF
-    fi
+  # Use official sources list if IP is not in China
+  if [ "$location" != "China" ]; then
+    URL="deb.debian.org/debian"
   else
-    # IP is not in China, update apt-get sources
-    echo "IP is not in China, updating apt-get sources."
-    # Use official sources list for Debian 6
-    if [[ $DEBIAN_VERSION == 6 ]]; then
-      cat > /etc/apt/sources.list <<EOF
-deb http://deb.debian.org/debian squeeze main contrib non-free
-deb-src http://deb.debian.org/debian squeeze main contrib non-free
-
-deb http://deb.debian.org/debian squeeze-updates main contrib non-free
-deb-src http://deb.debian.org/debian squeeze-updates main contrib non-free
-
-deb http://security.debian.org/ squeeze/updates main contrib non-free
-deb-src http://security.debian.org/ squeeze/updates main contrib non-free
-EOF
-    # Use official sources list for Debian 7
-    elif [[ $DEBIAN_VERSION == 7 ]]; then
-      cat > /etc/apt/sources.list <<EOF
-deb http://deb.debian.org/debian wheezy main contrib non-free
-deb-src http://deb.debian.org/debian wheezy main contrib non-free
-
-deb http://deb.debian.org/debian wheezy-updates main contrib non-free
-deb-src http://deb.debian.org/debian wheezy-updates main contrib non-free
-
-deb http://security.debian.org/ wheezy/updates main contrib non-free
-deb-src http://security.debian.org/ wheezy/updates main contrib non-free
-EOF
-   # Use official sources list for Debian 8
-    elif [[ $DEBIAN_VERSION == 8 ]]; then
-      cat > /etc/apt/sources.list <<EOF
-deb http://deb.debian.org/debian jessie main contrib non-free
-deb-src http://deb.debian.org/debian jessie main contrib non-free
-
-deb http://deb.debian.org/debian jessie-updates main contrib non-free
-deb-src http://deb.debian.org/debian jessie-updates main contrib non-free
-
-deb http://security.debian.org/ jessie/updates main contrib non-free
-deb-src http://security.debian.org/ jessie/updates main contrib non-free
-EOF
-    # Use official sources list for Debian 9
-    elif [[ $DEBIAN_VERSION == 9 ]]; then
-      cat > /etc/apt/sources.list <<EOF
-deb http://deb.debian.org/debian stretch main contrib non-free
-deb-src http://deb.debian.org/debian stretch main contrib non-free
-
-deb http://deb.debian.org/debian stretch-updates main contrib non-free
-deb-src http://deb.debian.org/debian stretch-updates main contrib non-free
-
-deb http://security.debian.org/ stretch/updates main contrib non-free
-deb-src http://security.debian.org/ stretch/updates main contrib non-free
-EOF
-    # Use official sources list for Debian 10
-    elif [[ $DEBIAN_VERSION == 10 ]]; then
-      cat > /etc/apt/sources.list <<EOF
-deb http://deb.debian.org/debian buster main contrib non-free
-deb-src http://deb.debian.org/debian buster main contrib non-free
-
-deb http://deb.debian.org/debian buster-updates main contrib non-free
-deb-src http://deb.debian.org/debian buster-updates main contrib non-free
-
-deb http://security.debian.org/ buster/updates main contrib non-free
-deb-src http://security.debian.org/ buster/updates main contrib non-free
-EOF
-    # Use official sources list for Debian 11
-    elif [[ $DEBIAN_VERSION == 11 ]]; then
-      cat > /etc/apt/sources.list <<EOF
-deb http://deb.debian.org/debian bullseye main contrib non-free
-deb-src http://deb.debian.org/debian bullseye main contrib non-free
-
-deb http://deb.debian.org/debian bullseye-updates main contrib non-free
-deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free
-
-deb http://security.debian.org/ bullseye/updates main contrib non-free
-deb-src http://security.debian.org/ bullseye/updates main contrib non-free
-EOF
-    fi
+    # Use mirrors.aliyun.com sources list if IP is in China
+    URL="mirrors.aliyun.com/debian"
   fi
+
+  # Set Debian release based on Debian version
+  case $DEBIAN_VERSION in
+    6.0) DEBIAN_RELEASE="squeeze";;
+    7.0) DEBIAN_RELEASE="wheezy";;
+    8.0) DEBIAN_RELEASE="jessie";;
+    9.0) DEBIAN_RELEASE="stretch";;
+    10.0) DEBIAN_RELEASE="buster";;
+    11.0) DEBIAN_RELEASE="bullseye";;
+    *) echo "The system is not Debian 6/7/8/9/10/11 . No changes were made to the apt-get sources." && return 1;;
+  esac
+  
+  # Write sources list in the desired format
+  cat > /etc/apt/sources.list <<EOF
+deb ${URL} ${DEBIAN_RELEASE} main contrib non-free
+deb ${URL} ${DEBIAN_RELEASE}-updates main contrib non-free
+deb ${URL} ${DEBIAN_RELEASE}-backports main contrib non-free
+deb http://security.debian.org/ ${DEBIAN_RELEASE}/updates main contrib non-free
+deb-src ${URL} ${DEBIAN_RELEASE} main contrib non-free
+deb-src ${URL} ${DEBIAN_RELEASE}-updates main contrib non-free
+deb-src ${URL} ${DEBIAN_RELEASE}-backports main contrib non-free
+deb-src http://security.debian.org/ ${DEBIAN_RELEASE}/updates main contrib non-free
+EOF
 }
 
 
@@ -173,91 +86,41 @@ change_ubuntu_apt_sources() {
   location=$(curl -s https://ipapi.co/$ip/country_name)
   
   # Check the system's Ubuntu version
-  ubuntu_version=$(lsb_release -r | awk '{print $2}')
-  if [ "$location" = "China" ]; then
-    # IP is in China, update apt-get sources
-    echo "IP is in China, updating apt-get sources."
-    # Backup the current sources.list
-    sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
-    if [ "$ubuntu_version" = "12.04" ]; then
-      # Write the AliYun Ubuntu 12.04 apt-get sources list to sources.list
-      echo "deb http://mirrors.aliyun.com/ubuntu/ precise main restricted universe multiverse" | sudo tee /etc/apt/sources.list
-      echo "deb http://mirrors.aliyun.com/ubuntu/ precise-security main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-      echo "deb http://mirrors.aliyun.com/ubuntu/ precise-updates main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-    elif [ "$ubuntu_version" = "14.04" ]; then
-      # Write the AliYun Ubuntu 14.04 apt-get sources list to sources.list
-      echo "deb http://mirrors.aliyun.com/ubuntu/ trusty main restricted universe multiverse" | sudo tee /etc/apt/sources.list
-      echo "deb http://mirrors.aliyun.com/ubuntu/ trusty-security main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-      echo "deb http://mirrors.aliyun.com/ubuntu/ trusty-updates main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-      echo "deb http://mirrors.aliyun.com/ubuntu/ trusty-backports main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-    elif [ "$ubuntu_version" = "16.04" ]; then
-      # Write the AliYun Ubuntu 16.04 apt-get sources list to sources.list
-      echo "deb http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse" | sudo tee /etc/apt/sources.list
-      echo "deb http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-      echo "deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-      echo "deb http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-    elif [ "$ubuntu_version" = "18.04" ]; then
-      # Write the AliYun Ubuntu 18.04 apt-get sources list to sources.list
-      echo "deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse" | sudo tee /etc/apt/sources.list
-      echo "deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-      echo "deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-      echo "deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-    elif [ "$ubuntu_version" = "20.04" ]; then
-      # Write the AliYun Ubuntu 20.04 apt-get sources list to sources.list
-      echo "deb http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse" | sudo tee /etc/apt/sources.list
-      echo "deb http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-      echo "deb http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-      echo "deb http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-    elif [ "$ubuntu_version" = "22.04" ]; then
-      # Write the AliYun Ubuntu 22.04 apt-get sources list to sources.list
-      echo "deb http://mirrors.aliyun.com/ubuntu/ groovy main restricted universe multiverse" | sudo tee /etc/apt/sources.list
-      echo "deb http://mirrors.aliyun.com/ubuntu/ groovy-security main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-      echo "deb http://mirrors.aliyun.com/ubuntu/ groovy-updates main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-      echo "deb http://mirrors.aliyun.com/ubuntu/ groovy-backports main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-    fi
-  else:
-    # Backup the current sources.list
-    sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
-    if [ "$ubuntu_version" = "12.04" ]; then
-      # Write the Ubuntu 12.04 apt-get sources list that can be used with apt-get to sources.list
-      echo "deb http://old-releases.ubuntu.com/ubuntu precise main restricted universe multiverse" | sudo tee /etc/apt/sources.list
-      echo "deb http://old-releases.ubuntu.com/ubuntu precise-security main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-      echo "deb http://old-releases.ubuntu.com/ubuntu precise-updates main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-    elif [ "$ubuntu_version" = "14.04" ]; then
-      # Write the standard Ubuntu 14.04 apt-get sources list to sources.list
-      echo "deb http://archive.ubuntu.com/ubuntu trusty main restricted universe multiverse" | sudo tee /etc/apt/sources.list
-      echo "deb http://archive.ubuntu.com/ubuntu trusty-security main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-      echo "deb http://archive.ubuntu.com/ubuntu trusty-updates main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-      echo "deb http://archive.ubuntu.com/ubuntu trusty-backports main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-    elif [ "$ubuntu_version" = "16.04" ]; then
-      # Write the standard Ubuntu 16.04 apt-get sources list to sources.list
-      echo "deb http://archive.ubuntu.com/ubuntu xenial main restricted universe multiverse" | sudo tee /etc/apt/sources.list
-      echo "deb http://archive.ubuntu.com/ubuntu xenial-security main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-      echo "deb http://archive.ubuntu.com/ubuntu xenial-updates main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-      echo "deb http://archive.ubuntu.com/ubuntu xenial-backports main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-    elif [ "$ubuntu_version" = "18.04" ]; then
-      # Write the standard Ubuntu 18.04 apt-get sources list to sources.list
-      echo "deb http://archive.ubuntu.com/ubuntu bionic main restricted universe multiverse" | sudo tee /etc/apt/sources.list
-      echo "deb http://archive.ubuntu.com/ubuntu bionic-security main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-      echo "deb http://archive.ubuntu.com/ubuntu bionic-updates main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-      echo "deb http://archive.ubuntu.com/ubuntu bionic-backports main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-    elif [ "$ubuntu_version" = "20.04" ]; then
-      # Write the standard Ubuntu 20.04 apt-get sources list to sources.list
-      echo "deb http://archive.ubuntu.com/ubuntu focal main restricted universe multiverse" | sudo tee /etc/apt/sources.list
-      echo "deb http://archive.ubuntu.com/ubuntu focal-security main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-      echo "deb http://archive.ubuntu.com/ubuntu focal-updates main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-      echo "deb http://archive.ubuntu.com/ubuntu focal-backports main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-    elif [ "$ubuntu_version" = "22.04" ]; then
-      # Write the standard Ubuntu 22.04 apt-get sources list to sources.list
-      echo "deb http://archive.ubuntu.com/ubuntu hirsute main restricted universe multiverse" | sudo tee /etc/apt/sources.list
-      echo "deb http://archive.ubuntu.com/ubuntu hirsute-security main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-      echo "deb http://archive.ubuntu.com/ubuntu hirsute-updates main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-      echo "deb http://archive.ubuntu.com/ubuntu hirsute-backports main restricted universe multiverse" | sudo tee -a /etc/apt/sources.list
-    else
-      echo "The system is not Ubuntu 12/14/16/18/20/22 . No changes were made to the apt-get sources."
-    fi
+  UBUNTU_VERSION=$(lsb_release -r | awk '{print $2}')
+
+  # Use official sources list if IP is not in China
+  if [ "$location" != "China" ]; then
+    URL="archive.ubuntu.com/ubuntu"
+  else
+    # Use mirrors.aliyun.com sources list if IP is in China
+    URL="mirrors.aliyun.com/ubuntu"
   fi
+
+  # Set Ubuntu release based on Ubuntu version
+  case $UBUNTU_VERSION in
+    # 14.04) UBUNTU_RELEASE="trusty";;
+    16.04) UBUNTU_RELEASE="xenial";;
+    18.04) UBUNTU_RELEASE="bionic";;
+    20.04) UBUNTU_RELEASE="focal";;
+    22.04) UBUNTU_RELEASE="groovy";;
+    *) echo "The system is not Ubuntu 14/16/18/20/22 . No changes were made to the apt-get sources." && return 1;;
+  esac
+  
+  # Backup current sources list
+  sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+  # Write sources list in the desired format
+  cat > /etc/apt/sources.list <<EOF
+deb ${URL} ${UBUNTU_RELEASE} main restricted universe multiverse
+deb ${URL} ${UBUNTU_RELEASE}-security main restricted universe multiverse
+deb ${URL} ${UBUNTU_RELEASE}-updates main restricted universe multiverse
+deb ${URL} ${UBUNTU_RELEASE}-backports main restricted universe multiverse
+deb-src ${URL} ${UBUNTU_RELEASE} main restricted universe multiverse
+deb-src ${URL} ${UBUNTU_RELEASE}-security main restricted universe multiverse
+deb-src ${URL} ${UBUNTU_RELEASE}-updates main restricted universe multiverse
+deb-src ${URL} ${UBUNTU_RELEASE}-backports main restricted universe multiverse
+EOF
 }
+
 
 
 check_eol_and_switch_apt_source() {
