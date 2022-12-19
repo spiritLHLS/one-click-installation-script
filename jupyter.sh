98e1627a005046a3f6935a9433272937a7e76822
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2022-12-19 11:36:25 +0800

    Update jupyter.sh

--- jupyter.sh
+++ jupyter.sh
@@ -4,12 +4,10 @@
 #version: 2022.12.19
 
 source ~/.bashrc
-
 red(){ echo -e "\033[31m\033[01m$1$2\033[0m"; }
 green(){ echo -e "\033[32m\033[01m$1$2\033[0m"; }
 yellow(){ echo -e "\033[33m\033[01m$1$2\033[0m"; }
 reading(){ read -rp "$(green "$1")" "$2"; }
-
 ver="2022.12.19"
 changeLog="一键安装jupyter环境"
 clear
@@ -24,7 +22,7 @@ echo "验证已支持的系统：
 echo "Ubuntu 18/20/22 - 推荐，脚本自动挂起到后台"
 echo "Debian 9/10/11 - 还行，需要手动挂起到后台，详看脚本运行安装完毕的后续提示"
 echo "可能支持的系统：centos 7+，Fedora，Almalinux 8.5+"
-red "本脚本尝试使用Miniconda3安装虚拟环境jupyter-env再进行jupyter和jupyterlab的安装，如若安装机器不纯洁勿要轻易使用本脚本！"
+red "本脚本尝试使用Miniconda3安装虚拟环境jupyter-env再进行jupyter和jupyterlab的安装，如若安装机器不纯净勿要轻易使用本脚本！"
 yellow "执行脚本，之前有用本脚本安装过则直接打印设置的登陆信息，没安装过则进行安装再打印信息"
 yellow "如果是初次安装无脑输入y回车即可，按照提示进行操作即可，安装完毕将在后台常驻运行"
 
