2aa1ac3a2550b22aec5214f369a367790e4a3285
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-06-16 22:47:03 +0800

    Update dlm.sh

--- install_scripts/dlm.sh
+++ install_scripts/dlm.sh
@@ -16,14 +16,15 @@ killall -9 AliHips >/dev/null 2>&1
 killall -9 AliYunDunUpdate >/dev/null 2>&1
 if [ -d /usr/local/aegis/aegis_debug ];then
     if [ -d /usr/local/aegis/aegis_debug/tracing/instances/aegis ];then
-	echo > /usr/local/aegis/aegis_debug/tracing/instances/aegis/set_event
+        echo > /usr/local/aegis/aegis_debug/tracing/instances/aegis/set_event
     else
         echo > /usr/local/aegis/aegis_debug/tracing/set_event
     fi
 fi
+
 if [ -d /sys/kernel/debug ];then
     if [ -d /sys/kernel/debug/tracing/instances/aegis ];then
-	echo > /sys/kernel/debug/tracing/instances/aegis/set_event
+        echo > /sys/kernel/debug/tracing/instances/aegis/set_event
     else
         echo > /sys/kernel/debug/tracing/set_event
     fi
@@ -31,33 +32,33 @@ fi
 if [ -d /usr/local/aegis ];then
     rm -rf /usr/local/aegis/aegis_client
     rm -rf /usr/local/aegis/aegis_update
-    rm -rf /usr/local/aegis/alihids
+	rm -rf /usr/local/aegis/alihids
 fi
 
 if [ -d /usr/local/aegis/aegis_debug ];then
     umount /usr/local/aegis/aegis_debug
     rm -rf /usr/local/aegis/aegis_debug
 fi
-   if [ -f "/etc/init.d/aegis" ]; then
-        /etc/init.d/aegis stop  >/dev/null 2>&1
-	rm -f /etc/init.d/aegis
-   fi
+if [ -f "/etc/init.d/aegis" ]; then
+    /etc/init.d/aegis stop  >/dev/null 2>&1
+    rm -f /etc/init.d/aegis
+fi
 
-   if [ $LINUX_RELEASE = "GENTOO" ]; then
-	rc-update del aegis default 2>/dev/null
-	if [ -f "/etc/runlevels/default/aegis" ]; then
-	    rm -f "/etc/runlevels/default/aegis" >/dev/null 2>&1;
-	fi
-    elif [ -f /etc/init.d/aegis ]; then
-        /etc/init.d/aegis  uninstall
-	for ((var=2; var<=5; var++)) do
-	    if [ -d "/etc/rc${var}.d/" ];then
-		rm -f "/etc/rc${var}.d/S80aegis"
-	    elif [ -d "/etc/rc.d/rc${var}.d" ];then
-	        rm -f "/etc/rc.d/rc${var}.d/S80aegis"
-	    fi
-	done
+if [ $LINUX_RELEASE = "GENTOO" ]; then
+    rc-update del aegis default 2>/dev/null
+    if [ -f "/etc/runlevels/default/aegis" ]; then
+        rm -f "/etc/runlevels/default/aegis" >/dev/null 2>&1;
     fi
+elif [ -f /etc/init.d/aegis ]; then
+    /etc/init.d/aegis  uninstall
+    for ((var=2; var<=5; var++)) do
+        if [ -d "/etc/rc${var}.d/" ];then
+                rm -f "/etc/rc${var}.d/S80aegis"
+        elif [ -d "/etc/rc.d/rc${var}.d" ];then
+            rm -f "/etc/rc.d/rc${var}.d/S80aegis"
+        fi
+    done
+fi
 /usr/local/cloudmonitor/CmsGoAgent.linux-${ARCH} stop && \
 /usr/local/cloudmonitor/CmsGoAgent.linux-${ARCH} uninstall && \
 rm -rf /usr/local/cloudmonitor
@@ -90,3 +91,37 @@ systemctl disable --now qemu-guest-agent
 service uma stop
 systemctl disable --now uma
 /usr/local/uniagent/extension/install/telescope/telescoped stop
+systemctl stop --no-block jcs-agent-core
+systemctl --no-reload disable jcs-agent-core
+stop --no-wait jcs-agent-core
+/etc/init.d/jcs-agent-core
+if [[ -f "/etc/centos-release" && $(grep ' 6' /etc/centos-release) ]];then
+    echo "Disable expand-root at startup ..."
+    chkconfig --level 2345 expand-root off
+
+    echo "Remove dracut growroot module ..."
+    rm -rf /usr/share/dracut/modules.d/50growroot
+
+    echo "Update dracut ..."
+    dracut --force
+
+    echo "Remove sgdisk tool in /usr/bin ..."
+    rm -f /usr/bin/sgdisk
+
+    echo "Remove growpart tool in /usr/bin ..."
+    rm -f /usr/bin/growpart
+fi
+systemctl stop --no-block jcs-shutdown-scripts
+systemctl stop --no-block jcs-entry
+systemctl --no-reload disable jcs-shutdown-scripts
+systemctl --no-reload disable jcs-entry
+stop --no-wait jcs-shutdown-scripts
+stop --no-wait jcs-entry
+service jcs-entry stop
+service jcs-shutdown-scripts stop
+chkconfig jcs-entry off
+chkconfig jcs-shutdown-scripts off
+update-rc.d jcs-entry remove
+update-rc.d jcs-shutdown-scripts remove
+pkill jdog
+rm -rf /usr/local/share/jcloud
